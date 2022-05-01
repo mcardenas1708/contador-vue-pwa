6f6ca4bad849649b3fd452ff4ccbc41e7049746f
mcardenas1708 <103071623+mcardenas1708@users.noreply.github.com>
2022-05-01 16:44:24 -0500

    V1 Contador App
    Aplicación final y desplegada

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: 'Contador App - Vue',
+            count: 0,
+        };
+    },
+    methods:{
+        /*disCount(){
+            this.count -= 1;
+        },
+        addCount(){
+            this.count  += 1;
+        },*/
+        modCount(instruction = 'add', limit = 1){
+            if (instruction === 'dis') this.count -= limit;
+            else this.count  += limit;
+        },
+    },
+});
